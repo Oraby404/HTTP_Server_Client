e2b315da5379fe55754d61ce219a38772a316c2c
mohamed saied <62272115+mohamedsaied87613@users.noreply.github.com>
2022-05-13 16:03:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,47 +4,49 @@ from _thread import *
 
 
 def ThreadedClient(cnx):
-    print("start")
-    # msg=''
-    # cnx.send('Thank you for connecting\n'.encode())
     cnx.settimeout(15)
     while True:
         data = cnx.recv(2048)  # receive request
         msg = data.decode('ascii')
         msg = msg.split('\r\n')  # split header and filter empty strings
-        msg = list(filter(None, msg))  # print("before", msg)
+        msg = list(filter(None, msg))
         print(msg)
 
+        # check if packet format is correct get/post line with mandatory fields
         if len(msg) and len(msg[0].split()) > 2:
-            print("after")
             if msg[0].split()[0] == "GET":
-                print("get")
+                print("GET REQUEST")
                 filename = msg[0].split()[1][1:]  # get file name
+                version = msg[0].split()[-1]  # get version
                 entries = os.listdir()  # entries in dir
                 search = entries.count(filename)  # search for filename
-                if search:
-                    print("HTTP/1.0 200 OK\r\n")
-                    cnx.send("HTTP/1.0 200 OK\r\n".encode())
-
+                if search:  #if file is on server
                     f = open(filename, "rb")
-                    #size = os.path.getsize(filename)
-                    file = f.read()
-                    cnx.send(file)
+                    file = f.read().decode('ascii')
                     f.close()
+                    packet = "{} 200 OK\r\n{}\r\n".format(version, file)
+                    print(packet)
+                    cnx.send(packet.encode())
                 else:
-                    print("HTTP/1.0 404 Not Found\r\n")
-                    cnx.send("HTTP/1.0 404 Not Found\r\n".encode())
+                    print(version, " 404 Not Found\r\n")
+                    packet = "{} 404 Not Found\r\n".format(version)
+                    cnx.send(packet.encode())
 
             elif msg[0].split()[0] == "POST":
-                print("post")
-                cnx.send('OK'.encode())
+                print("POST REQUEST")
                 filename = msg[0].split()[1][1:]  # get file name
+                version = msg[0].split()[-1]  # get version
                 f = open(filename, 'wb')  # Open in binary
-                #file = cnx.recv(8096)
-                f.write(str.encode(msg[-1]))
+                try:
+                    f.write(str.encode(msg[-1]))
+                    packet = "{} 200 OK".format(version)
+                    cnx.send(packet.encode())
+                except:
+                    cnx.send("{} 400 Bad Request".format(version).encode())
+
                 f.close()
         else:
-            cnx.send("ERROR IN PACKET".encode())
+            cnx.send("PACKET NOT VALID".encode())
 
 
 # cnx.close()
