d3107dbde70ab6c33d961cc5c9006d627dc2a407
mohamed saied <62272115+mohamedsaied87613@users.noreply.github.com>
2022-05-13 04:28:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,55 +4,56 @@ from _thread import *
 
 
 def ThreadedClient(cnx):
-    cnx.timeout(15)
+    print("start")
+    # msg=''
+    # cnx.send('Thank you for connecting\n'.encode())
+    cnx.settimeout(15)
     while True:
-        data = cnx.recv(1024)
-
-        msg = (str(data)[2:-1].split('\\r\\n'))  # split header and filter empty strings
-        msg = list(filter(None, msg))
-
+        data = cnx.recv(2048)  # receive request
+        msg = data.decode('ascii')
+        msg = msg.split('\r\n')  # split header and filter empty strings
+        msg = list(filter(None, msg))  # print("before", msg)
         print(msg)
 
-        if len(msg) > 1:
+        if len(msg) and len(msg[0].split()) > 2:
+            print("after")
             if msg[0].split()[0] == "GET":
+                print("get")
                 filename = msg[0].split()[1][1:]  # get file name
                 entries = os.listdir()  # entries in dir
                 search = entries.count(filename)  # search for filename
                 if search:
+                    print("HTTP/1.0 200 OK\r\n")
                     cnx.send("HTTP/1.0 200 OK\r\n".encode())
-                    f = open(filename, "rb")
 
-                    file = f.read(1024)
-                    while file:
-                        cnx.send(file)
-                        file = f.read(1024)
+                    f = open(filename, "rb")
+                    #size = os.path.getsize(filename)
+                    file = f.read()
+                    cnx.send(file)
+                    f.close()
                 else:
+                    print("HTTP/1.0 404 Not Found\r\n")
                     cnx.send("HTTP/1.0 404 Not Found\r\n".encode())
 
             elif msg[0].split()[0] == "POST":
-                filename = msg[0].split()[1][1:]  # get file name
+                print("post")
                 cnx.send('OK'.encode())
-
+                filename = msg[0].split()[1][1:]  # get file name
                 f = open(filename, 'wb')  # Open in binary
-                while True:
-                    file = cnx.recv(1024)
-                    f.write(file)
-                    if not file:
-                        break
+                #file = cnx.recv(8096)
+                f.write(str.encode(msg[-1]))
                 f.close()
         else:
-            cnx.send("ERROR".encode())
-
-        if not data:  # when client end cnx
-            print('CLIENT ENDED CONNECTION')
-            break
-    cnx.close()
+            cnx.send("ERROR IN PACKET".encode())
 
 
-s = socket.socket()
+# cnx.close()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 threads = 0
-s.bind(('', 65432))
-s.listen(20)
+s.bind(('', 55555))
+s.listen(5)
 
 while True:
     c, addr = s.accept()
